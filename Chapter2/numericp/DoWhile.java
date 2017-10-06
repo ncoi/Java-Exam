96519ef45af7f4c532c01d0812ca5524196e1a7f
Nelson Orduz <norduz@konradgroup.com>
2017-10-06 17:13:40 -0600

    chapter2

---
+++ Chapter2/numericp/DoWhile.java
@@ -0,0 +1,13 @@
+package numericp;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int i = 2;
+		do {
+			i--;
+			System.out.println("1 vez");
+		} while(i == 1);
+	}
+
+}
---
+++ Chapter2/numericp/NumProm.java
@@ -0,0 +1,22 @@
+package numericp;
+
+public class NumProm {
+	static byte b = 1;
+	static char c = 'a';
+	static short s = 2;
+	static int i = 3;
+	static int i2 = 3;
+	static long l = 4;
+	static double d = 1.0;
+	
+	public static void main(String[] args) {
+		long l1 = b + l;
+		System.out.println(l1);
+		
+		short x = 1;
+		float y = 3;
+		double z = 3;
+		System.out.println(x * y / z);
+	}
+
+}
